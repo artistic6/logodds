24bd5cd5f5146054f6a26507fea407292ea72246
artistic6 <artisticsoftware6@gmail.com>
2023-02-12 03:57:35 +0000

    ...

---
+++ 0212/0212.php
@@ -0,0 +1,27 @@
+<?php
+
+/**	Most Common Occurence */	
+/**	Number 5 shows 3 Times */
+/**	Second Most Common Occurence */	
+/**	Number 12 shows 1 Times */
+
+return [
+	'Race 1' => 
+	[
+		'Win' => [12, 5, 1]
+	],
+	'Race 2' => 
+	[
+		'Win' => [5, 7, 3]
+	],
+	'Race 6' => 
+	[
+		'Win' => [5, 9, 2]
+	],
+	'Race 10' => 
+	[
+		'Win' => [5, 6, 11]
+	],
+];
+
+?>
